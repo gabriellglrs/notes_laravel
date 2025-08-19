ce030a0d3ae5571241fa60d70755d3ae8d7e9a4d
gabriellglrs <gabriellglrs@gmail.com>
2025-08-19 14:35:30 -0300

    refactor: Fazendo a Limpeza do Projeto

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,16 +7,4 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index($id) {
-        $nome = "gabriel";
-        return view('welcome')->with('id', $id);
-    }
-
-    public function sobre() {
-        return view('sobre');
-    }
-
-    public function contato() {
-        return view('contato');
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/sobre', [MainController::class, 'sobre']);
-
-Route::get('/contato', [MainController::class, 'contato']);

8173bd84b223aa200ca6a63a42c3d728b52d1e1e
gabriellglrs <gabriellglrs@gmail.com>
2025-08-19 15:38:52 -0300

    feat: Implementa sistema de autenticação inicial
    - Remove o MainController que não estava em uso.
    - Adiciona o AuthController para gerenciar o login e logout de usuários.
    - Cria as rotas /login e /logout e as associa ao AuthController.
    - Adiciona as views e assets necessários para a renderização da página de login.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view("login");
+    }
+
+    public function logout()
+    {
+        echo "logout";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
 
+Route::get("/login", [AuthController::class, "login"]);
+Route::get("/logout", [AuthController::class,"logout"]);
 

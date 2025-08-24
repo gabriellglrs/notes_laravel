149954d57347afd7fd653257bde64d5f04d5e74e
gabriellglrs <gabriellglrs@gmail.com>
2025-08-24 12:41:09 -0300

    feat: Adiciona middleware para proteger rotas de autenticação
    - Adiciona o middleware `CheckIsNotLogged` para redirecionar usuários logados que tentarem acessar a página de login.
    - Aplica o middleware `CheckIsNotLogged` às rotas de login e de submissão de login.
    - Reorganiza os grupos de rotas para separar as rotas de autenticação das rotas principais da aplicação.

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 
-// Auth Routes
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+// Auth Routes - usuario nao logado
+Route::middleware([CheckIsNotLogged::class])->group(function () {
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
 
+// Main Routes - usuario logado
 Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index']);
-    Route::get('/teste', [MainController::class,'teste']);
+    Route::get('/teste', [MainController::class, 'teste']);
     Route::get('/logout', [AuthController::class, 'logout']);
 });
